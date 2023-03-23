daeaa86e27407adf9d55149909fe40491c00ad8e
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-23 14:26:26 +0100

    esercizio base + bonus concluso

--- js/main.js
+++ js/main.js
@@ -10,7 +10,17 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+        randomMail: []
     }
+  },
+  mounted: function(){
+    for(let i = 0; i <10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((result) =>{
+            this.randomMail.push(result.data.response);
+        });
+    }
+    console.log(this.randomMail);
+
   }
 }).mount('#app')
